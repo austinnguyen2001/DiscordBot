b140e86e058f83029354ea7f58e9c0f0bc1de0e0
Austin Nguyen <austinnguyen01@gmail.com>
2019-12-08 19:29:13 -0800

    botjs fixes

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 import config from './config.json';
 import { Client, Collection } from 'discord.js';
+import fs from 'fs';
 
 const client = new Client();
 client.commands = new Collection();
@@ -16,15 +17,26 @@ for(let i = 0, len = commandFiles.length; i < len; i++) {
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! \nIf you haven't yet added the bot, press the link below`);
     console.log(`https://discordapp.com/oauth2/authorize?client_id=${client.user.toString().match(/\d/g).join("")}&scope=bot`);
-    client.user.setActivity("Patrolling da server", {type: "WATCHING"});
 });
 
 client.on('message', msg => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
+    if(msg.author.bot) return;
+    if(msg.channel.type === "dm") return;
 
     if (msg.content === 'ping') {
         msg.reply('pong');
+    }
+    // just messing around
+    if (msg.content === 'getReactions') {
+        const filter = (reaction, user) => {
+            console.log(user.id);
+            console.log(msg.author.id);
+            return reaction.emoji.name === '👌' && user.id === msg.author.id;
+        };
+        
+        msg.awaitReactions(filter, { time: 15000 })
+            .then(collected => console.log(`Collected ${collected.size} reactions`))
+            .catch(console.error);
     }
 });
   
